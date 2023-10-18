e8eee749aa66c9446cdb0394cfd2417170a027d5
jambi <stevenjugushh@gmail.com>
2023-10-18 12:19:33 +0300

    activated the delete functionality

--- static/javascript/products.js
+++ static/javascript/products.js
@@ -5,60 +5,71 @@ $(document).ready(function () {
 });
 
 
+// Function to delete a row with confirmation
+// $(document).on('click', '.delete_link i', function () {
+//     if (confirm("Confirm delete")) {
+//         const row = $(this).closest('tr');
+//         if (row) {
+//             const productId = row.data('product-id'); // Get the product ID from the row's data attribute
 
-// function to delete the product in product table
-$(document).on('click', '.actionRemove_btn', function () {
-    var productId = $(this).data('product-id');
-    var deleteUrl = $(this).data('delete-url');
-    if (confirm("Proceed to remove")) {
-        $.ajax({
-            type: 'DELETE',
-            url: deleteUrl,
-            success: function (data) {
-                alert(data.message); 
-                location.reload(); // Or window.location.href = window.location.href;
-            },
-            error: function (error) {
-                console.error(error);
-            }
-        });
+//             // Send an AJAX request to delete the product on the server
+//             $.ajax({
+//                 type: 'DELETE',
+//                 url: '/products/delete_product/' + productId + '/',
+//                 success: function () {
+//                     // Remove the row from the table
+//                     row.remove();
+//                 },
+//                 error: function (error) {
+//                     console.error(error);
+//                 }
+//             });
+//         }
+//     }
+// });
+
+// Function to delete a row with confirmation
+$(document).on('click', '.delete_link i', function () {
+    if (confirm("Confirm delete")) {
+        const row = $(this).closest('tr');
+        if (row) {
+            const productId = row.data('product-id');
+            const table = $('#product_table').DataTable(); // Get the DataTable
+
+            // Send an AJAX request to delete the product on the server
+            $.ajax({
+                type: 'DELETE',
+                url: '/products/delete_product/' + productId + '/',
+                success: function () {
+                    // Remove the row from the table
+                    row.remove();
+
+                    // Update the DataTable to reorder the rows
+                    table.draw();
+                },
+                error: function (error) {
+                    console.error(error);
+                }
+            });
+        }
     }
 });
 
-
-// function to Edit the product in product table
-
-    // getting all the edit buttons 
-    const editButtons = document.querySelectorAll(".edit_button");
-
-    // Events Listeners 
-    editButtons.forEach(buttons => {
-        buttons.addEventListener("click", function(){
-
-        
-        // show the edit from pop
-        const editForm = document.querySelector('.editForm_hidden');
-        editForm.computedStyleMap.display = 'block';
-
-        // fill the edit form fields with the row data 
-        const row = this.closest('tr');
-        const productType = row.cells[1].textContent;
-        const productName = row.cells[2].textContent;
-        const productPrice = row.cells[3].textContent;
-
-        document.getElementById('editProductType').value = productType;
-        document.getElementById('editProductName').value = productName;
-        document.getElementById('editProductPrice').value = productPrice;
-
-
-
-    });
-
+// Function to edit the product in the product table
+$(document).on('click', '.edit_link i', function () {
+    // Handle editing functionality here
+    // You can show an edit form or implement your editing logic
+    // For example, you can display a popup with the edit form.
 });
 
-// Add event listener to the "save" button.
-const saveButton = document.getElementById("actionEdit_btn2");
-saveButton.addEventListener("click", function(){
-    const editForm = document.querySelector(".editForm_hidden");
-    editForm.style.display = "none";
-})
+// Function to initialize the DataTable
+$(document).ready(function () {
+    $('#product_table').DataTable();
+});
+
+
+
+
+
+
+
